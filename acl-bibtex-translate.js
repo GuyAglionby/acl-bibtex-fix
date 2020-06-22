a229a09af362461fd50de44beebe1ed8d3b52ec2
Guy Aglionby <guy@guyaglionby.com>
2020-06-22 12:04:05 +0100

    formatting

--- acl-bibtex-translate.js
+++ acl-bibtex-translate.js
@@ -40,7 +40,7 @@ const diffClasses = {
     }
 };
 
-name_types = ['first', 'middle', 'last', 'prelast', 'lineage']
+name_types = ['first', 'middle', 'last', 'prelast', 'lineage'];
 
 // date formats
 const dateFormats = [
@@ -56,8 +56,8 @@ const dateFormats = [
 const monthFormats = [
     "MM",
     "MMM",
-    "MMMM"
-]
+    "MMMM",
+];
 
 function inputElementOnChange(elem) { 
     // https://stackoverflow.com/questions/16215771/how-open-select-file-dialog-via-js/16215950
@@ -77,8 +77,8 @@ function download() {
     let bibtexString = toBibtex(translatedEntries, false);
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(bibtexString));
-    let downloadFilename = bibtexFilename.substring(0, bibtexFilename.length - '.bib'.length); 
-    downloadFilename += '-acl-fixed.bib'
+    let downloadFilename = bibtexFilename.substring(0, bibtexFilename.length - '.bib'.length);
+    downloadFilename += '-acl-fixed.bib';
     element.setAttribute('download', downloadFilename);
     element.style.display = 'none';
     document.body.appendChild(element);
@@ -110,10 +110,10 @@ function simplifyTitle(title) {
     }
     title = title.toLowerCase();
     Object.keys(reverseMapping).forEach(function(source) {
-        title = title.replace(source, reverseMapping[source])
+        title = title.replace(source, reverseMapping[source]);
     });
     title = title.normalize("NFD").replace(diacriticRegex, "");
-    return [...title.matchAll(simplifyRegex)].join(' ')
+    return [...title.matchAll(simplifyRegex)].join(' ');
 }
 
 function convert() {
@@ -129,11 +129,11 @@ function convert() {
                 delete newEntry.entryTags.eprinttype;
                 delete newEntry.entryTags.journaltitle;
             }
-            newEntry.entryType = anthEntry.bibType
-            newEntry.entryTags.title = anthEntry.title
-            newEntry.entryTags.pages = anthEntry.pages
+            newEntry.entryType = anthEntry.bibType;
+            newEntry.entryTags.title = anthEntry.title;
+            newEntry.entryTags.pages = anthEntry.pages;
             if (anthEntry.bibType == 'inproceedings') {
-                newEntry.entryTags.booktitle = anthEntry.booktitle
+                newEntry.entryTags.booktitle = anthEntry.booktitle;
             }
             for (let possTag of ["doi", "pages", "publisher"]) {
                 if (possTag in anthEntry) {
@@ -151,7 +151,7 @@ function convert() {
             if ("month" in anthEntry) {
                 if ("date" in newEntry.entryTags) {
                     let existingDate = matchDate(newEntry.entryTags.date, dateFormats);
-                    let anthMonth = matchDate(anthEntry.month, monthFormats)
+                    let anthMonth = matchDate(anthEntry.month, monthFormats);
                     if (existingDate.isValid() && anthMonth.isValid()) {
                         // Only change existing dates if they're wrong (don't change format just for the sake of it)
                         if (existingDate.year() != anthEntry.year || existingDate.month()  != anthMonth.month()) {
@@ -169,7 +169,7 @@ function convert() {
                     newEntry.entryTags.year = anthEntry.year;
                 }
             } else if ("date" in anthEntry) {
-                newEntry.entryTags.date = anthEntry.date
+                newEntry.entryTags.date = anthEntry.date;
             } else{
                 console.warn("No date info available for title :" + strippedTitle);
             }
@@ -191,8 +191,8 @@ function convert() {
  * https://github.com/ORCID/bibtexParseJs/blob/master/bibtexParse.js#L323-L354
  */
 function toDiffedBibtex(orig, modified) {
-    let parentElem = document.createElement('table')
-    parentElem.className += 'result'
+    let parentElem = document.createElement('table');
+    parentElem.className += 'result';
     const entrysep = ',';
     const indent = '        ';
 
@@ -216,20 +216,20 @@ function toDiffedBibtex(orig, modified) {
     if (tagSeq.includes('date') && tagSeq.includes('month') && tagSeq.includes('year')) {
         tagSeq = tagSeq.filter(tag => tag != 'year' && tag != 'month');
         let dateIndex = tagSeq.indexOf('date') + 1;
-        tagSeq.splice(dateIndex, 0, 'year')
-        tagSeq.splice(dateIndex, 0, 'month')
+        tagSeq.splice(dateIndex, 0, 'year');
+        tagSeq.splice(dateIndex, 0, 'month');
     }
     if (tagSeq.includes('booktitle') && tagSeq.includes('journaltitle')) {
         let first = tagSeq.indexOf('booktitle') < tagSeq.indexOf('journaltitle') ? 'booktitle' : 'journaltitle';
         let second = first == 'booktitle' ? 'journaltitle' : 'booktitle';
         tagSeq = tagSeq.filter(tag => tag != second);
         let dateIndex = tagSeq.indexOf(first) + 1;
-        tagSeq.splice(dateIndex, 0, second)
+        tagSeq.splice(dateIndex, 0, second);
     }
     if (tagSeq.includes('booktitle') && tagSeq.includes('publisher')) {
         tagSeq = tagSeq.filter(tag => tag != 'publisher');
         let dateIndex = tagSeq.indexOf('booktitle') + 1;
-        tagSeq.splice(dateIndex, 0, 'publisher')
+        tagSeq.splice(dateIndex, 0, 'publisher');
     }
     let numFieldsAdded = 0;
     for (let field of tagSeq) {
@@ -240,7 +240,7 @@ function toDiffedBibtex(orig, modified) {
             if (modEntry == origEntry) {
                 let addedText = indent + field + ' = {' + modEntry + '}';
                 addedText += shouldComma ? ',' : '';
-                parentElem.appendChild(rowWithText(addedText))
+                parentElem.appendChild(rowWithText(addedText));
             } else {
                 let modAdded = indent + field + ' = {' + modEntry;
                 modAdded += shouldComma ? ',' : '';
@@ -248,7 +248,7 @@ function toDiffedBibtex(orig, modified) {
                 origAdded += shouldComma ? ',' : '';
                 doDiffLine(origAdded, modAdded, parentElem);
             }
-        } else if (field in modified.entryTags){ 
+        } else if (field in modified.entryTags) {
             let addedText = indent + field + ' = {' + modified.entryTags[field] + '}';
             addedText += shouldComma ? ',' : '';
             parentElem.appendChild(rowWithText(addedText, diffClasses['added']['bg']));
@@ -259,7 +259,7 @@ function toDiffedBibtex(orig, modified) {
         }
         numFieldsAdded += 1;
     }
-    parentElem.appendChild(rowWithText('}'))
+    parentElem.appendChild(rowWithText('}'));
     return parentElem;
 }
 
@@ -271,25 +271,28 @@ function toDiffedBibtex(orig, modified) {
 function toBibtex(json, compact) {
     if (compact === undefined) compact = true;
     var out = '';
-    
+
     var entrysep = ',';
     var indent = '';
     if (!compact) {
-                  entrysep = ',\n';
-                  indent = '    ';        
+      entrysep = ',\n';
+      indent = '    ';
     }
-    for ( var i in json) {
+    for (var i in json) {
         out += "@" + json[i].entryType;
         out += '{';
-        if (json[i].citationKey)
+        if (json[i].citationKey) {
             out += json[i].citationKey + entrysep;
-        if (json[i].entry)
-            out += json[i].entry ;
+        }
+        if (json[i].entry) {
+            out += json[i].entry;
+        }
         if (json[i].entryTags) {
             var tags = indent;
             for (var jdx in json[i].entryTags) {
-                if (tags.trim().length != 0)
+                if (tags.trim().length != 0) {
                     tags += entrysep + indent;
+                }
                 tags += jdx + (compact ? '={' : ' = {') + 
                         json[i].entryTags[jdx] + '}';
             }
@@ -298,8 +301,7 @@ function toBibtex(json, compact) {
         out += compact ? '}\n' : '\n}\n\n';
     }
     return out;
-
-};
+}
 
 // ########################################## 
 // # Helper functions for rendering diffs 
@@ -307,15 +309,15 @@ function toBibtex(json, compact) {
 
 function diffedSpan(pre, changed, post, highlight_clazz, whole_clazz) {
     let parentSpan = document.createElement('span');
-    parentSpan.appendChild(spanWithText(pre))
-    parentSpan.appendChild(spanWithText(changed, highlight_clazz))
-    parentSpan.appendChild(spanWithText(post))
+    parentSpan.appendChild(spanWithText(pre));
+    parentSpan.appendChild(spanWithText(changed, highlight_clazz));
+    parentSpan.appendChild(spanWithText(post));
     parentSpan.className += whole_clazz;
     return parentSpan;
 }
 
 function spanWithText(text, clazz) {
-    let s = document.createElement('span')
+    let s = document.createElement('span');
     s.textContent = text;
     if (!!clazz) {
         s.className += clazz;
@@ -328,10 +330,10 @@ function rowWithText(text, clazz) {
 }
 
 function rowWithElem(elem, clazz) {
-    let r = document.createElement('tr')
-    let d = document.createElement('td')
-    r.appendChild(d)
-    d.appendChild(elem)
+    let r = document.createElement('tr');
+    let d = document.createElement('td');
+    r.appendChild(d);
+    d.appendChild(elem);
     if (!!clazz) {
         r.className += clazz;
     }
@@ -353,7 +355,7 @@ function makeDiffLineElem(diff, prop) {
 function doDiffLine(origLine, modLine, parentElem) {
     let diff = Diff.diffWords(origLine, modLine);
     parentElem.appendChild(makeDiffLineElem(diff, 'removed')) 
-    parentElem.appendChild(makeDiffLineElem(diff, 'added'))
+    parentElem.appendChild(makeDiffLineElem(diff, 'added'));
 }
 
 // ########################################## 
